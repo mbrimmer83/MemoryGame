d8259794afe062ebf535bade73eea83e2d5ad27f
Matthew <mbrimmer1@gmail.com>
2016-05-19 22:16:44 -0400

    The memory game is almost complete

--- memorygame.js
+++ memorygame.js
@@ -4,9 +4,70 @@ var state = 'on';
 var lastMonster = '';
 var currentMonster = '';
 var matchCounter = 0;
+var levelNumber = 16;
 
+var monsters = [
+  'monsters-01.png',
+  'monsters-02.png',
+  'monsters-03.png',
+  'monsters-04.png',
+  'monsters-05.png',
+  'monsters-06.png',
+  'monsters-07.png',
+  'monsters-08.png',
+  'monsters-09.png',
+  'monsters-10.png',
+  'monsters-11.png',
+  'monsters-12.png',
+  'monsters-13.png',
+  'monsters-14.png',
+  'monsters-15.png',
+  'monsters-16.png'
+];
+
+function randomMonster(levelNumber) {
+  var monstersDeck1 = [];
+  var monstersDeck2 = [];
+  for (var i = 0; i < levelNumber; i++) {
+    var idx = Math.floor(Math.random() * monsters.length);
+    monstersDeck1.push(monsters[idx]);
+    monstersDeck1.push(monsters[idx]);
+    monsters.splice(idx, 1);
+  }
+  while (monstersDeck1.length > 0) {
+    var idx2 = Math.floor(Math.random() * monstersDeck1.length);
+    monstersDeck2.push(monstersDeck1[idx2]);
+    monstersDeck1.splice(idx2, 1);
+  }
+
+  return monstersDeck2;
+}
+
+function addMonster() {
+  var monstersDeck = randomMonster(levelNumber);
+  var html = '';
+  var columnNum = "";
+  if (levelNumber === 4) {
+    columnNum = "column-4";
+  } else if (levelNumber === 9) {
+    columnNum = "column-6";
+  } else if (levelNumber === 16) {
+    columnNum = "column-8";
+  }
+  for (var i = 0; i < monstersDeck.length; i++) {
+    var monster = monstersDeck[i];
+    html += ('<div class="tile">' +
+    '<img class="monster" src="../memorygame/images/' + monster + '">' +
+    '<div class="back"></div>' +
+    '</div>');
+  }
+  $('#grid')
+  .addClass(columnNum)
+  .append(html);
+}
 
 $(function() {
+  addMonster ();
   $('.tile').click(function() {
     if ($(this).hasClass('show')) {
       return;
@@ -39,7 +100,7 @@ $(function() {
       $('#message').text('Sorry, that is not a match!')
     }
     $('#turn').text('Turn: ' + moveCounter);
-    if (matchCounter === 4) {
+    if (matchCounter === levelNumber) {
       $('#message').text('Winner');
     }
   });
